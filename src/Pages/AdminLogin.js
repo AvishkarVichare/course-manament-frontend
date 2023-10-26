7eff4122c782ff79184a60f12c9e32d45c26629c
Avishkar Vichare <avie9500@gmail.com>
2023-10-26 20:07:39 +0530

    removed admin route

--- src/Pages/AdminLogin.js
+++ src/Pages/AdminLogin.js
@@ -66,9 +66,7 @@ const AdminLogin = () => {
                      
                   </div>
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-primary-800">Sign in</button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <Link to="/admin/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
-                  </p>
+                
               </form>
           </div>
       </div>
